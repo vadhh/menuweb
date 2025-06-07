07733468ed6bc0c06dc2b2f4a6172ce9adba4224
DHOOM <149752468+vadhh@users.noreply.github.com>
2025-06-07 21:51:49 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig = {
-  typescript: {
-
-    ignoreBuildErrors: true,
-  },
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
