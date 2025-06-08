a12fdcdae5dd2730e5267052e6f46f5b6854994f
vadhh <attridho@gmail.com>
2025-06-08 09:33:31 +0700

    fix login deployment 2

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,29 @@
 import { type NextAuthConfig } from "next-auth";
 import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
+import { User } from '@/app/models/user'; // Import your User model
+import bcrypt from 'bcryptjs'; // Import bcryptjs for password comparison
+import connectMongoDB  from '@/lib/mongodb'; // Assuming this is where connectMongoDB is
 
 export const authOptions: NextAuthConfig = {
   providers: [
-    // Removed Google provider for simpler login
+    Credentials({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" }, // Changed to email for consistency with your User model
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        await connectMongoDB(); // Ensure database connection
+        const user = await User.findOne({ email: credentials.email });
+
+        if (user && (await bcrypt.compare(credentials.password as string, user.password))) {
+          // Return user object if authentication is successful
+          return { id: user._id.toString(), name: user.email, email: user.email }; // You might want to add more user details here
+        }
+        return null; // Return null if user is not found or credentials are invalid
+      },
+    }),
   ],
   callbacks: {
     async session({ session, token }) {
